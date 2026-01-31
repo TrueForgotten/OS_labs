df077d1296e0a36b0d0a367470332337978056ee
TrueForgotten <notoriginalname13@gmail.com>
2026-01-31 23:30:02 +1000

    lab5

---
+++ build/CMakeFiles/4.1.1/CMakeRCCompiler.cmake
@@ -0,0 +1,6 @@
+set(CMAKE_RC_COMPILER "C:/Users/bubub/Desktop/study/os_labs/lab5/rep/w64devkit/bin/windres.exe")
+set(CMAKE_RC_COMPILER_ARG1 "")
+set(CMAKE_RC_COMPILER_LOADED 1)
+set(CMAKE_RC_SOURCE_FILE_EXTENSIONS rc;RC)
+set(CMAKE_RC_OUTPUT_EXTENSION .obj)
+set(CMAKE_RC_COMPILER_ENV_VAR "RC")
